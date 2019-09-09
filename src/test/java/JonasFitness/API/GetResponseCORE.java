1de3678d2d4a09975c033a8dec16a71f1d3eb565
tnuzum <todd.nuzum@jonasfitness.com>
2019-09-09 13:44:09 -0400

    added sample CORE call

--- src/test/java/JonasFitness/API/GetResponseCORE.java
+++ src/test/java/JonasFitness/API/GetResponseCORE.java
@@ -3,34 +3,31 @@ package JonasFitness.API;
 import static io.restassured.RestAssured.given;
 
 import java.io.IOException;
-
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import resources.ReusableMethods;
 import resources.base;
 import resources.resources;
 
-public class GetResponse extends base {
+public class GetResponseCORE extends base {
 
+
+    
 	@BeforeTest
 	public void getData() throws IOException {
 		base.getPropertyData();
+		
 	}
 
 	@Test
 	public void Test1() {
-		RestAssured.baseURI = prop.getProperty("hostG");
+		RestAssured.baseURI = prop.getProperty("http://compete-ws.test-jfisoftware.net:4412/Info/CustomerInfo.svc");
 		Response res = 
 				
 				given().
-					param("location", "40.128354, -83.017916").
-					param("radius", "500").
-					param("key", prop.getProperty("keyG")).
-					param("type", "restaurant").
+					param("barcodeId", "4890").
 
 				when().get(resources.getnearbyDataJSON()).
 				then().
@@ -42,9 +39,9 @@ public class GetResponse extends base {
 		System.out.println(responseString);
 
 		// ** Used to get specific element from response **
-		JsonPath js = ReusableMethods.rawToJson(res);
-		String name = js.get("results[0].name");
-		System.out.println(name);
+//		JsonPath js = ReusableMethods.rawToJson(res);
+//		String name = js.get("results[0].name");
+//		System.out.println(name);
 	}
 
 }
