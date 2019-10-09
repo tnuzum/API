8d4c46b6598a47b176cb4abd7a2290173f1b0b68
tnuzum <todd.nuzum@jonasfitness.com>
2019-10-09 15:37:32 -0400

    authenticate member added to test suite

--- src/test/java/JonasFitness/API/AuthenticateMemberByUserCredentials.java
+++ src/test/java/JonasFitness/API/AuthenticateMemberByUserCredentials.java
@@ -10,33 +10,28 @@ import java.util.concurrent.TimeUnit;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-public class PostAuthenticateMember {
+public class AuthenticateMemberByUserCredentials {
 
 	@Test
 	public void Test1() {
 		RestAssured.useRelaxedHTTPSValidation();
 		RestAssured.baseURI = ("https://compete-api-future2.test-jfisoftware.com:8252");
 
-		Response res =		
-			given()
-			.header("accept", "application/json")
+			given().log().all()
 			.header("X-Api-Key", "B50A8F2BF7315812CF2A21690A7FF5FDA33A156C")
 			.header("X-CompanyId", "101")
 			.header("X-ClubId", "1")
-			.header("Content-Type", "application/json-patch+json")
+			.header("Content-Type", "application/json")
 			.when()
 				.body("{"+
-						  "\"Username\": \"zauto\","+
-						  "\"Password\": \"5669\""+
-						"}").
-			when()
+						  "\"Username\": \"rauto\","+
+						  "\"Password\": \"Testing1!\""+
+						"}")
+			.when()
 				.post("/api/v3/member/authenticatememberbyusercredentials").
 			then().log().body()
 			.assertThat().statusCode(200)
 			.time(lessThan(5L),TimeUnit.SECONDS).extract().response();
-		
-//		String responseString = res.asString();
-//		System.out.println(responseString);
 		
 			
 	}
