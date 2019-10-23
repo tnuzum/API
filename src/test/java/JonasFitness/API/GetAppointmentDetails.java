2f9ea8197e8a0c4e2d2f1e4c2eb9c7c944aed92a
tnuzum <todd.nuzum@jonasfitness.com>
2019-10-22 20:36:22 -0400

    adding assertions

--- src/test/java/JonasFitness/API/GetAppointmentDetails.java
+++ src/test/java/JonasFitness/API/GetAppointmentDetails.java
@@ -88,7 +88,18 @@ public class GetAppointmentDetails extends base {
 						.body("Result.BookedResources[2]", hasKey("ResourceTypeDescription"))
 						.body("Result.BookedResources[2]", hasKey("ResourceTypeId"))
 						.body("Result.BookedResources[2]", hasKey("ResourceTypeName"))
+						.body("Result.Club", hasKey("ClubId"))
+						.body("Result.Club", hasKey("ClubName"))
+						
+						.body("Result.Club.Details", hasKey("AppointmentDateTime"))
+						.body("Result.Club.Details", hasKey("AppointmentDuration"))
+						.body("Result.Club.Details", hasKey("AppointmentNotes"))
+						.body("Result.Club.Details.CanCancel", hasKey("CanCancel"))
+						.body("Result.Club.Details.CanCancel", hasKey("CancellationReason"))
+						.body("Result.Club.Details", hasKey("CancelDate"))
+						.body("Result.Club.Details", hasKey("DateAppointmentCreated"))
 
+// Assert values returned are correct	
 						
 						.body("Result.BookedMembers[0].AppointmentCharge", equalTo(0))
 						.body("Result.BookedMembers[0].AppointmentOutcome", equalTo("Future"))
@@ -116,34 +127,7 @@ public class GetAppointmentDetails extends base {
 						.body("Result.BookedMembers[1].LastName", equalTo("Auto"))
 						.body("Result.BookedMembers[1].NoShowFee", equalTo(5))
 						.body("Result.BookedMembers[1].NoShowFeeIndicator", equalTo(false))
-						/*
-						 *     "BookedResources": [
-		      {
-		        "BookDescription": "Employee, Golf Instructor, Personal Trainer",
-		        "BookId": 221,
-		        "BookName": "Campbell, Samual",
-		        "ResourceTypeDescription": "",
-		        "ResourceTypeId": 147,
-		        "ResourceTypeName": "Golf Instructors"
-		      },
-		      {
-		        "BookDescription": "",
-		        "BookId": 226,
-		        "BookName": "Driving Range 2",
-		        "ResourceTypeDescription": "Used for Golf Lessons",
-		        "ResourceTypeId": 148,
-		        "ResourceTypeName": "Golf Practice Areas"
-		      },
-		      {
-		        "BookDescription": "Rental Golf Clubs used for Golf Lessons",
-		        "BookId": 224,
-		        "BookName": "Golf Clubs",
-		        "ResourceTypeDescription": "Used for Golf Lessons",
-		        "ResourceTypeId": 149,
-		        "ResourceTypeName": "Golf Equipment"
-		      }
-		    ],
-						 */
+				
 						.body("Result.BookedResources[0].BookDescription", equalTo("Employee, Golf Instructor, Personal Trainer"))
 						.body("Result.BookedResources[0].BookId", equalTo(221))
 						.body("Result.BookedResources[0].BookName", equalTo("Campbell, Samual"))
@@ -162,9 +146,18 @@ public class GetAppointmentDetails extends base {
 						.body("Result.BookedResources[2].ResourceTypeDescription", equalTo("Used for Golf Lessons"))
 						.body("Result.BookedResources[2].ResourceTypeId", equalTo(149))
 						.body("Result.BookedResources[2].ResourceTypeName", equalTo("Golf Equipment"))
+						.body("Result.Club.ClubId", equalTo(1))
+						.body("Result.Club.ClubName", equalTo("Club Number One"))
+						
+						.body("Result.Club.Details.AppointmentDateTime", equalTo("2019-10-25T16:00:00-04:00"))
+						.body("Result.Club.Details.AppointmentDuration", equalTo(60))
+						.body("Result.Club.Details.AppointmentNotes", equalTo("MSS Appointment"))
+						.body("Result.Club.Details.CanCancel.CanCancel", equalTo(true))
+						.body("Result.Club.Details.CanCancel.CancellationReason", equalTo("NoCancellationFeeApplies"))
+						.body("Result.Club.Details.CancelDate", nullValue())
+						.body("Result.Club.Details.DateAppointmentCreated", equalTo("2019-10-18T14:21:30.303-04:00"))
+						
 						;			
-				
-
 	}
 	@Test (testName="AppointmentsNotFound",description="PBI:139310")
 	public void AppointmentsNotFound() {
