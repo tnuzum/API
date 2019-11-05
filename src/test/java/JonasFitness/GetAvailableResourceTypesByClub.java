1fe578168709ba4d385a0402a0865008fef8c94b
bhagyakalle <rayaprolub@gmail.com>
2019-11-04 21:37:30 -0500

    Assertions for Data values- getActiveClubs and getActiveBooksByResourceType

---
+++ src/test/java/JonasFitness/GetAvailableResourceTypesByClub.java
@@ -0,0 +1,54 @@
+package JonasFitness;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.lessThan;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import resources.ReusableMethods;
+import resources.base;
+
+public class GetAvailableResourceTypesByClub extends base{
+
+	@BeforeTest
+	public void getData() throws IOException {
+		base.getPropertyData();
+		RestAssured.useRelaxedHTTPSValidation();
+		RestAssured.baseURI = prop.getProperty("baseURI");
+	}
+	
+	@Test (testName="ValidInput",description="PBI:138961")
+	public void ValidInput() {
+
+				given()
+//						.log().all()
+				.header("accept", prop.getProperty("accept"))
+				.header("X-Api-Key", prop.getProperty("X-Api-Key"))
+				.header("X-CompanyId", prop.getProperty("X-CompanyId"))
+				.header("X-ClubId", prop.getProperty("X-ClubId"))
+					.when()
+						.get("/api/v3/bookview/getavailableresourcetypesbyclub")
+						.then()
+//						.log().body()
+						.assertThat().statusCode(200)
+						.time(lessThan(5L),TimeUnit.SECONDS)
+						.body("Result[0]", hasKey("ResourceTypeDescription"))
+						.body("Result[0]", hasKey("ResourceTypeId"))
+						.body("Result[0]", hasKey("ResourceTypeName"))
+						.body("Result[1]", hasKey("ResourceTypeDescription"))
+						.body("Result[1]", hasKey("ResourceTypeId"))
+						.body("Result[1]", hasKey("ResourceTypeName"))
+						.body("Result[2]", hasKey("ResourceTypeDescription"))
+						.body("Result[2]", hasKey("ResourceTypeId"))
+						.body("Result[2]", hasKey("ResourceTypeName"));
+	}
+}
---
+++ src/test/java/JonasFitness/GetOnlinePackagesForPurchaseByClub.java
@@ -0,0 +1,56 @@
+package JonasFitness;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import java.io.IOException;
+import io.restassured.RestAssured;
+import resources.base;
+
+public class GetOnlinePackagesForPurchaseByClub extends base {
+	
+	@BeforeTest
+	public void getData() throws IOException {
+		base.getPropertyData();
+		RestAssured.useRelaxedHTTPSValidation();
+		RestAssured.baseURI = prop.getProperty("baseURI");
+	}
+	@Test (testName="PackagesFound",description="PBI:143537")
+	public void PackagesFound() { 
+
+				given()
+//						.log().all()
+				.header("accept", prop.getProperty("accept"))
+				.header("X-Api-Key", prop.getProperty("X-Api-Key"))
+				.header("X-CompanyId", prop.getProperty("X-CompanyId"))
+				.header("X-ClubId", prop.getProperty("X-ClubId"))
+					.when()
+						.get("/api/v3/package/getonlinepackagesforpurchasebyclub/29947/1")
+						.then()
+//						.log().body()
+						.assertThat().statusCode(200)
+//						.time(lessThan(5L),TimeUnit.SECONDS)
+						;
+	}
+	@Test (testName="PackageNotAllowed",description="PBI:143537")
+	public void PackageNotAllowed() { 
+		
+// this is not found because the item is not allowed for MSS (online) purchase
+		
+				given()
+//						.log().all()
+				.header("accept", prop.getProperty("accept"))
+				.header("X-Api-Key", prop.getProperty("X-Api-Key"))
+				.header("X-CompanyId", prop.getProperty("X-CompanyId"))
+				.header("X-ClubId", prop.getProperty("X-ClubId"))
+					.when()
+						.get("/api/v3/package/getonlinepackagesforpurchasebyclub/29947/1")
+						.then()
+//						.log().body()
+						.assertThat().statusCode(200)
+//						.time(lessThan(5L),TimeUnit.SECONDS)
+						;
+// need to assert that a package that is not allowed for MSS purchase is not contained in the response
+	}
+}
