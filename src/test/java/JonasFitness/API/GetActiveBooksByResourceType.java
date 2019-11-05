1e1e6cf525312b1a584e1259f711705929db19ac
bhagyakalle <rayaprolub@gmail.com>
2019-11-05 17:07:10 -0500

    GetActiveBooksByResourceType

--- src/test/java/JonasFitness/API/GetActiveBooksByResourceType.java
+++ src/test/java/JonasFitness/API/GetActiveBooksByResourceType.java
@@ -6,7 +6,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.lessThan;
-import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -29,26 +29,26 @@ public class GetActiveBooksByResourceType extends base{
 	@Test (testName="ValidInput_NoResource",description="PBI:138964")
 	public void ValidInput_NoResource() {
 		
-		String resourceTypeId = prop.getProperty("resourceType1Id");
+		String resourceTypeId = prop.getProperty("resourceType4Id");
 
 				given()
-//						.log().all()
+					.log().all()
 						.header("accept", prop.getProperty("accept"))
 						.header("X-Api-Key", prop.getProperty("X-Api-Key"))
 						.header("X-CompanyId", prop.getProperty("X-CompanyId"))
-						.header("X-ClubId", prop.getProperty("X-ClubId"))
+						.header("X-ClubId", prop.getProperty("X-ClubId1"))
 						.queryParam("ResourceTypeId", resourceTypeId)
 					.when()
 						.get("/api/v3/bookview/getactivebooksbyresourcetype")
 						.then()
-//						.log().body()
+					.log().body()
 						.assertThat().statusCode(200)
 						.time(lessThan(5L),TimeUnit.SECONDS)
 						.body("Result[0]", hasKey("BookType"))
 						.body("Result[0]", hasKey("Description"))
 						.body("Result[0]", hasKey("Id"))
-						.body("Result[0]", hasKey("Name"));
-//						.body("Result[]", containsString("Bhagya") );
+						.body("Result[0]", hasKey("Name"))
+					.body("Result.Name[0]", equalTo("Kalle, Bhagya") );
 
 	}
 }
