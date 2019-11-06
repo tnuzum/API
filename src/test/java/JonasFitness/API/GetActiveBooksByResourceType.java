27308928f654e49b070038a7f074f75a0d73079d
bhagyakalle <rayaprolub@gmail.com>
2019-11-05 22:00:44 -0500

    Update GetActiveBooksByResourceType.java
    3rd time

--- src/test/java/JonasFitness/API/GetActiveBooksByResourceType.java
+++ src/test/java/JonasFitness/API/GetActiveBooksByResourceType.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.Matchers.equalTo;
-//import static org.hamcrest.Matchers.hasItemInArray;
+import static org.hamcrest.Matchers.hasItemInArray;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -49,8 +49,8 @@ public class GetActiveBooksByResourceType extends base{
 						.body("Result[0]", hasKey("Description"))
 						.body("Result[0]", hasKey("Id"))
 						.body("Result[0]", hasKey("Name"))
-					.body("Result.Name[0]", equalTo("Kalle, Bhagya") );
-					
+					.body("Result.Name[0]", equalTo("Kalle, Bhagya") )
+					.body("Result", hasItemInArray("{Description=Kalle Bhagya, BookType=Staff, Id=214, Name=Kalle, Bhagya}"));
 
 	}
 }
