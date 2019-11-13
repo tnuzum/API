9779ff4ceb11a295a0e2203b16da65985d4d42b5
tnuzum <todd.nuzum@jonasfitness.com>
2019-11-13 09:52:58 -0500

    enabled test 'with resource'

--- src/test/java/JonasFitness/API/GetActiveBooksByResourceType.java
+++ src/test/java/JonasFitness/API/GetActiveBooksByResourceType.java
@@ -55,7 +55,7 @@ public class GetActiveBooksByResourceType extends base{
 						.body("Result[0]", hasKey("Name"));
 	}
 	
-	/*@Test (testName="ValidInput_WithResource",description="PBI:138964")
+	@Test (testName="ValidInput_WithResource",description="PBI:138964")
 	public void ValidInput_WithResource() {
 		
 		String resourceTypeId = prop.getProperty("resourceType4Id");
@@ -65,7 +65,7 @@ public class GetActiveBooksByResourceType extends base{
 						.header("accept", prop.getProperty("accept"))
 						.header("X-Api-Key", prop.getProperty("X-Api-Key"))
 						.header("X-CompanyId", prop.getProperty("X-CompanyId"))
-						.header("X-ClubId", prop.getProperty("X-Club1Id1"))
+						.header("X-ClubId", prop.getProperty("X-Club1Id"))
 						.queryParam("ResourceTypeId", resourceTypeId)
 					.when()
 						.get("/api/v3/bookview/getactivebooksbyresourcetype")
@@ -77,10 +77,11 @@ public class GetActiveBooksByResourceType extends base{
 						.body("Result[0]", hasKey("Description"))
 						.body("Result[0]", hasKey("Id"))
 						.body("Result[0]", hasKey("Name"))
-					    .body("Result[0].Name", equalTo("Kalle, Bhagya") )
-					    .body("Result[1].Name", equalTo("Kalle1, Bhagya1") )
-					    .body("Result.Name", anyOf(hasItem("Kalle, Bhagya")))
-					    .body("Result.Name", anyOf(not(hasItem("Kalle, Prabhat"))));
-	}*/
+//					    .body("Result[0].Name", equalTo("Kalle, Bhagya") )
+//					    .body("Result[1].Name", equalTo("Kalle1, Bhagya1") )
+//					    .body("Result.Name", anyOf(hasItem("Kalle, Bhagya")))
+//					    .body("Result.Name", anyOf(not(hasItem("Kalle, Prabhat"))))
+						;
+	}
 	
 }
