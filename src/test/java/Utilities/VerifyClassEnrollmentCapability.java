ab5ba7efd917d389db194d06a81000c19e4dbe33
tnuzum <todd.nuzum@jonasfitness.com>
2019-12-17 18:25:49 -0500

    updated date range

--- src/test/java/Utilities/VerifyClassEnrollmentCapability.java
+++ src/test/java/Utilities/VerifyClassEnrollmentCapability.java
@@ -4,19 +4,12 @@ import static io.restassured.RestAssured.given;
 
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import static org.hamcrest.Matchers.*;
-
 import java.io.IOException;
 import io.restassured.RestAssured;
 import resources.base;
 
 public class VerifyClassEnrollmentCapability extends base{
 	
-	/*
-	 * This call is not shown in Swagger
-	 * because it's hidden from integrators
-	 */
-
 	@BeforeTest
 	public void getData() throws IOException {
 		base.getPropertyData();
@@ -24,25 +17,25 @@ public class VerifyClassEnrollmentCapability extends base{
 		RestAssured.baseURI = prop.getProperty("baseURI");
 	}
 	
-	@Test (testName="Enrollment Not Allowed",description="PBI:150003")
-	public void enrollmentNotAllowed() {
+	@Test ()
+	public void verifyClassEnrollmentCapability() {
  
-		String companyId 		= prop.getProperty("X-CompanyId");
-		String clubId 			= prop.getProperty("X-Club1Id");
-		int customerId 			= 223;
-		String classBarcodeId 	= "BalanceItem";
+		int companyId = 236;
+		int clubId = 1;
+		int customerId = 223;
+		String classBarcodeId 	= "standby001";
 		String classOccurrence 	= "2022-12-06";
-		String displayedClassPrice	= "15.00";
+		String displayedClassPrice	= "0.00";
 
 				given()
-//						.log().all()
-				.header("accept", prop.getProperty("accept"))
-				.header("X-Api-Key", prop.getProperty("X-Api-Key"))
-				.header("X-CompanyId", prop.getProperty("X-CompanyId"))
-				.header("X-ClubId", prop.getProperty("X-Club1Id"))
-					.when()
-						.get("/api/v3/enrollmentcapability/verifyclassenrollmentcapability/"+companyId+"/"+clubId+"/"+customerId+"/"+classBarcodeId+"/"+classOccurrence+"/"+displayedClassPrice)
-						.then()
-						.log().body();
+				.log().all()
+					.header("accept", prop.getProperty("accept"))
+					.header("X-Api-Key", prop.getProperty("X-Api-Key"))
+					.header("X-CompanyId", prop.getProperty("X-CompanyId"))
+					.header("X-ClubId", prop.getProperty("X-Club1Id"))
+				.when()
+						.get("/api/v3/enrollmentcapability/verifyclassenrollmentcapability/"+companyId+"/"+clubId+"/"+customerId+"/"
+								+classBarcodeId+"/"+classOccurrence+"/"+displayedClassPrice)
+					.then().log().body();
 	}
 }
