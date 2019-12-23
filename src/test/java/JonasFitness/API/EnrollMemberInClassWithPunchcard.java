09ac57f40f52eeb3c974bb710c7b87bc8c7d7375
tnuzum <todd.nuzum@jonasfitness.com>
2019-12-23 16:27:07 -0500

    2 new tests added

---
+++ src/test/java/JonasFitness/API/EnrollMemberInClassWithPunchcard.java
@@ -0,0 +1,149 @@
+package JonasFitness.API;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.*;
+
+import java.io.IOException;
+import io.restassured.RestAssured;
+import resources.base;
+
+public class EnrollMemberInClassWithPunchcard extends base {
+	
+	@BeforeTest
+	public void getData() throws IOException {
+		base.getPropertyData();
+		RestAssured.useRelaxedHTTPSValidation();
+		RestAssured.baseURI = prop.getProperty("baseURI");
+	}
+	/*
+	// !!! Disabled until an unenroll is created
+	@Test (testName="Member Enrolled - Class Already Started",description="PBI:147808")
+	public void memberEnrolledClassStarted() {
+		
+				int customerId = 248;
+				String classBarcodeId = "alwaysAvailCl";
+				String classOccurrence = "2025-12-31";
+				String enrollCustomerAsStandby = "true";
+
+				given()
+//						.log().all()
+				.header("accept", prop.getProperty("accept"))
+				.header("X-Api-Key", prop.getProperty("X-Api-Key"))
+				.header("X-CompanyId", prop.getProperty("X-CompanyId"))
+				.header("X-ClubId", prop.getProperty("X-Club1Id"))
+					.when()
+						.get("/api/v3/classcourse/enrollmemberinclasswithpunchcard/"+customerId+"/"+classBarcodeId+"/"+classOccurrence+"/"+enrollCustomerAsStandby+"")
+						.then()
+//						.log().body()
+						.assertThat().statusCode(200)
+//						.time(lessThan(5L),TimeUnit.SECONDS)
+						.body("Result.Enrolled", equalTo(true))
+						.body("Result.EnrollmentStatus", equalTo("Enrolled"))
+						.body("Result.CustomerId", equalTo(customerId))
+						.body("Result.FirstName", not(nullValue()))
+						.body("Result.LastName", not(nullValue()))
+						.body("Result", hasKey("MiddleInitial"))
+						.body("Result.DisplayName", not(nullValue()))
+						.body("Result.PreferredName", not(nullValue()));
+	}
+	 
+		@Test (testName="Member Enrolled - Class Already Not Started",description="PBI:147808")
+	public void memberEnrolledClassNotStarted() {
+		
+				int customerId = 248;
+				String classBarcodeId = "notStartedCl";
+				String classOccurrence = "2119-12-25";
+				String enrollCustomerAsStandby = "true";
+
+				given()
+				.header("accept", prop.getProperty("accept"))
+				.header("X-Api-Key", prop.getProperty("X-Api-Key"))
+				.header("X-CompanyId", prop.getProperty("X-CompanyId"))
+				.header("X-ClubId", prop.getProperty("X-Club1Id"))
+					.when()
+					.get("/api/v3/classcourse/enrollmemberinclasswithpunchcard/"+customerId+"/"+classBarcodeId+"/"+classOccurrence+"/"+enrollCustomerAsStandby+"")
+						.then()
+//						.log().body()
+						.assertThat().statusCode(200)
+						.body("Result.Enrolled", equalTo(true))
+						.body("Result.EnrollmentStatus", equalTo("Enrolled"))
+						.body("Result.CustomerId", equalTo(customerId))
+						.body("Result.FirstName", not(nullValue()))
+						.body("Result.LastName", not(nullValue()))
+						.body("Result", hasKey("MiddleInitial"))
+						.body("Result.DisplayName", not(nullValue()))
+						.body("Result.PreferredName", not(nullValue()));
+	}
+		
+	@Test (testName="Member Enrolled On Standby",description="PBI:147808")
+	public void memberEnrolledOnStandby() {
+		
+				int customerId 			= 248;
+				String classBarcodeId 	= "standbyCl";
+				String classOccurrence 	= "2025-12-31";
+				String enrollCustomerAsStandby = "true";
+
+				given()
+				.header("accept", prop.getProperty("accept"))
+				.header("X-Api-Key", prop.getProperty("X-Api-Key"))
+				.header("X-CompanyId", prop.getProperty("X-CompanyId"))
+				.header("X-ClubId", prop.getProperty("X-Club1Id"))
+					.when()
+					.get("/api/v3/classcourse/enrollmemberinclasswithpunchcard/"+customerId+"/"+classBarcodeId+"/"+classOccurrence+"/"+enrollCustomerAsStandby+"")
+						.then()
+//						.log().body()
+						.assertThat().statusCode(200)
+						.body("Result.Enrolled", equalTo(false))
+						.body("Result.EnrollmentStatus", equalTo("StandBy"))
+						.body("Result.CustomerId", equalTo(customerId))
+						.body("Result.FirstName", not(nullValue()))
+						.body("Result.LastName", not(nullValue()))
+						.body("Result", hasKey("MiddleInitial"))
+						.body("Result.DisplayName", not(nullValue()))
+						.body("Result.PreferredName", not(nullValue()));
+	}
+	*/
+	@Test (testName="Not Enough Punches",description="PBI:147808")
+	public void notEnoughPunches() {
+		
+				int customerId = 247;
+				String classBarcodeId = "notStartedCl";
+				String classOccurrence 	= "2119-12-25";
+				String enrollCustomerAsStandby = "true";
+
+				given()
+				.header("accept", prop.getProperty("accept"))
+				.header("X-Api-Key", prop.getProperty("X-Api-Key"))
+				.header("X-CompanyId", prop.getProperty("X-CompanyId"))
+				.header("X-ClubId", prop.getProperty("X-Club1Id"))
+					.when()
+						.get("/api/v3/classcourse/enrollmemberinclasswithpunchcard/"+customerId+"/"+classBarcodeId+"/"+classOccurrence+"/"+enrollCustomerAsStandby+"")
+						.then()
+//						.log().body()
+						.assertThat().statusCode(400)
+						.body("Message", equalTo("NotEnoughPunches"));
+		}
+	@Test (testName="Punchcard Not Allowed",description="PBI:147808")
+	public void punchcardNotAllowed() {
+		
+				int customerId = 247;
+				String classBarcodeId = "noPunchCl";
+				String classOccurrence 	= "2025-12-31";
+				String enrollCustomerAsStandby = "true";
+
+				given()
+				.header("accept", prop.getProperty("accept"))
+				.header("X-Api-Key", prop.getProperty("X-Api-Key"))
+				.header("X-CompanyId", prop.getProperty("X-CompanyId"))
+				.header("X-ClubId", prop.getProperty("X-Club1Id"))
+					.when()
+						.get("/api/v3/classcourse/enrollmemberinclasswithpunchcard/"+customerId+"/"+classBarcodeId+"/"+classOccurrence+"/"+enrollCustomerAsStandby+"")
+						.then()
+//						.log().body()
+						.assertThat().statusCode(400)
+						.body("Message", equalTo("EnrollmentNotAllowed - ItemRestrictions"));
+		}
+	}
