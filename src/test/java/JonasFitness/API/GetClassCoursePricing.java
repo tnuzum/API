e80a95cecb49c259cad679a2fde247441fad079a
tnuzum <todd.nuzum@jonasfitness.com>
2020-01-13 16:07:12 -0500

    Update GetClassCoursePricing.java

--- src/test/java/JonasFitness/API/GetClassCoursePricing.java
+++ src/test/java/JonasFitness/API/GetClassCoursePricing.java
@@ -8,6 +8,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.is;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -24,7 +25,38 @@ public class GetClassCoursePricing extends base {
 		RestAssured.baseURI = prop.getProperty("baseURI");
 	}
 	
-	@Test (testName="Item Found - Multiple Taxes",description="PBI:TBD")
+	@Test (testName="Item Found - Single Tax",description="PBI:155543")
+	public void itemFound_SingleTax() { 
+		
+		int customerId = 248;
+		int itemId = 250;
+
+				given()
+//						.log().all()
+				.header("accept", prop.getProperty("accept"))
+				.header("X-Api-Key", prop.getProperty("X-Api-Key"))
+				.header("X-CompanyId", prop.getProperty("X-CompanyId"))
+				.header("X-ClubId", prop.getProperty("X-Club1Id"))
+					.when()
+						.get("/api/v3/classcourse/getclasscoursepricing/"+customerId+"/"+itemId)
+						.then()
+//						.log().body()
+						.assertThat().statusCode(200)
+						.time(lessThan(5L),TimeUnit.SECONDS)
+						.body("Result.CanPlaceOnAccount", equalTo(true))
+						.body("Result.GrandTotal", not(nullValue()))
+						.body("Result.PriceDetails[0].CorrelationId", not(nullValue()))
+						.body("Result.PriceDetails[0].CustomerId", equalTo(customerId))
+						.body("Result.PriceDetails[0].IsTaxed", equalTo(true))
+						.body("Result.PriceDetails[0].ItemId", equalTo(itemId))
+						.body("Result.PriceDetails[0].Price", not(nullValue()))
+						.body("Result.SubTotal", not(nullValue()))
+						.body("Result.Tax", not(nullValue()))
+						.body("Result.TaxDetails[0].TaxAmount", not(nullValue()))
+						.body("Result.TaxDetails[0].TaxItemId", not(nullValue()));
+	}
+	
+	@Test (testName="Item Found - Multiple Taxes",description="PBI:155543")
 	public void itemFound_MultipleTaxes() { 
 		
 		int customerId = 248;
@@ -39,7 +71,7 @@ public class GetClassCoursePricing extends base {
 					.when()
 						.get("/api/v3/classcourse/getclasscoursepricing/"+customerId+"/"+itemId)
 						.then()
-						.log().body()
+//						.log().body()
 						.assertThat().statusCode(200)
 						.time(lessThan(5L),TimeUnit.SECONDS)
 						.body("Result.CanPlaceOnAccount", equalTo(true))
@@ -73,5 +105,63 @@ public class GetClassCoursePricing extends base {
 						.body("Result.TaxDetails[2].TaxItemId", equalTo(6))
 						.body("Result.TaxDetails[3].TaxAmount", equalTo(0.35))
 						.body("Result.TaxDetails[3].TaxItemId", equalTo(7)) */
+	}
+	
+	@Test (testName="Item Found - No Tax",description="PBI:155543")
+	public void itemFound_NoTax() { 
+		
+		int customerId = 248;
+		int itemId = 224;
+
+				given()
+//						.log().all()
+				.header("accept", prop.getProperty("accept"))
+				.header("X-Api-Key", prop.getProperty("X-Api-Key"))
+				.header("X-CompanyId", prop.getProperty("X-CompanyId"))
+				.header("X-ClubId", prop.getProperty("X-Club1Id"))
+					.when()
+						.get("/api/v3/classcourse/getclasscoursepricing/"+customerId+"/"+itemId)
+						.then()
+						.log().body()
+						.assertThat().statusCode(200)
+						.time(lessThan(5L),TimeUnit.SECONDS)
+						.body("Result.CanPlaceOnAccount", equalTo(true))
+						.body("Result.GrandTotal", not(nullValue()))
+						.body("Result.PriceDetails[0].CorrelationId", not(nullValue()))
+						.body("Result.PriceDetails[0].CustomerId", equalTo(customerId))
+						.body("Result.PriceDetails[0].IsTaxed", equalTo(false))
+						.body("Result.PriceDetails[0].ItemId", equalTo(itemId))
+						.body("Result.PriceDetails[0].Price", not(nullValue()))
+						.body("Result.SubTotal", not(nullValue()))
+						.body("Result.Tax", equalTo(0));
+	}
+	
+	@Test (testName="Item Found - Free Item",description="PBI:155543")
+	public void itemFound_FreeItem() { 
+		
+		int customerId = 248;
+		int itemId = 246;
+
+				given()
+//						.log().all()
+				.header("accept", prop.getProperty("accept"))
+				.header("X-Api-Key", prop.getProperty("X-Api-Key"))
+				.header("X-CompanyId", prop.getProperty("X-CompanyId"))
+				.header("X-ClubId", prop.getProperty("X-Club1Id"))
+					.when()
+						.get("/api/v3/classcourse/getclasscoursepricing/"+customerId+"/"+itemId)
+						.then()
+						.log().body()
+						.assertThat().statusCode(200)
+						.time(lessThan(5L),TimeUnit.SECONDS)
+						.body("Result.CanPlaceOnAccount", equalTo(true))
+						.body("Result.GrandTotal", is(0))
+						.body("Result.PriceDetails[0].CorrelationId", not(nullValue()))
+						.body("Result.PriceDetails[0].CustomerId", equalTo(customerId))
+						.body("Result.PriceDetails[0].IsTaxed", equalTo(false))
+						.body("Result.PriceDetails[0].ItemId", equalTo(itemId))
+						.body("Result.PriceDetails[0].Price", is(0))
+						.body("Result.SubTotal", not(nullValue()))
+						.body("Result.Tax", is(0));
 	}
 }
