425643b1529b0aecd093df26b18c396b7e6e990e
Todd Nuzum <52832004+tnuzum@users.noreply.github.com>
2020-07-23 09:46:27 -0400

    diableded logging and assertion for message

--- src/test/java/unitTests/PurchasePackageWithNewCreditCard.java
+++ src/test/java/unitTests/PurchasePackageWithNewCreditCard.java
@@ -531,7 +531,7 @@ public class PurchasePackageWithNewCreditCard extends base{
 				.body(PackagePL.PurchasePackageWithNewCreditCard(customerId,itemId,quantity,displayedGrandTotal,cardNumber,nameOnCard,month,year,securityCode,addressLine1,city,state,postalCode))
 				.post("/api/v3/package/purchasepackagewithnewcreditcard")
 			.then()
-				//.log().body()
+//				.log().body()
 				.statusCode(200)
 				.time(lessThan(60L),TimeUnit.SECONDS)
 				.body("Status", equalTo(200))
